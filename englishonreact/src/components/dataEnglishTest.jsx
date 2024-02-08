d998f0de70438557f2da133e48e5ef70be8e1ebd
antvolkov84 <antvolkov84@gmail.com>
2024-02-08 22:57:21 +0200

    add base of questions by english

--- englishonreact/src/components/dataEnglishTest.jsx
+++ englishonreact/src/components/dataEnglishTest.jsx
@@ -6,4 +6,33 @@ export const dataEnglishTest = [
     rightAnswer: 'is cooking',
     img: '../englishImg/first.jpg',
   },
+  {
+    id: 2,
+    question: 'Do you hear? ____a baby_________right now?',
+    answers: ['am/crying', 'is / crying', 'are/ crying', 'crying'],
+    rightAnswer: 'is / crying',
+    img: '../englishImg/second.jpg',
+  },
+  {
+    id: 3,
+    question:
+      'Help your father. He _____________a loaf of bread at the moment.',
+    answers: ['is cutting', 'cutting', 'are cutting', 'cuts'],
+    rightAnswer: 'is cutting',
+    img: '../englishImg/third.jpg',
+  },
+  {
+    id: 4,
+    question: 'Look! This young lady____________vey well now!',
+    answers: ['dances', 'is dance', 'dance', 'is dancing'],
+    rightAnswer: 'is dance',
+    img: '../englishImg/forth.jpg',
+  },
+  {
+    id: 5,
+    question: 'They __________wonderful pictures in the classroom right now.',
+    answers: ['am drawing', 'drawing', 'is drawing', 'are drawing'],
+    rightAnswer: 'are drawing',
+    img: '../englishImg/fifth.jpg',
+  },
 ];
